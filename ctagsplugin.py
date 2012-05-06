a2f5aaa722001227977e22a5d8aae7d9fcaf9bd1
Yury Korolev <yury.korolev@gmail.com>
2012-05-07 03:03:06 +0400

    Fix typo

--- ctagsplugin.py
+++ ctagsplugin.py
@@ -590,7 +590,7 @@ class AutocompleteAll(sublime_plugin.EventListener):
     def on_query_completions(self, view, prefix, locations):
         window = sublime.active_window()
         # get results from each tab
-        results = [v.extract_completions(prefix) for v in window.views() #if v.buffer_id() != view.buffer_id()]
+        results = [v.extract_completions(prefix) for v in window.views()] #if v.buffer_id() != view.buffer_id()]
         results = [(item,item) for sublist in results for item in sublist] #flatten
 
         # get results from tags
