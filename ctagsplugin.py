03223f2dda0d38031f317237467200ebcbf52c98
Cameron Eagans <cweagans@gmail.com>
2012-12-17 03:58:26 -0700

    Replacing old autocomplete logic with http://webdesktoplife.com/post/17601816246/sublime-text-autocomplete

--- ctagsplugin.py
+++ ctagsplugin.py
@@ -675,15 +675,18 @@ class rebuild_tags(sublime_plugin.TextCommand):
 ################################# AUTOCOMPLETE #################################
 
 class CTagsAutoComplete(sublime_plugin.EventListener):
-     def on_query_completions(self, view, prefix, locations):
-         tags = find_tags_relative_to(view.file_name())
-         completions = []
-
-         if tags:
-             tag_file = TagFile(tags, SYMBOL, MATCHES_STARTWITH)
-             completions = [(a,a) for a in sorted(tag_file.get_tags_dict(prefix[0]))]
-
-         return []
+    def on_query_completions(self, view, prefix, locations):
+        tags_path = view.window().folders()[0]+"/.tags"
+        results=[]
+        if (not view.window().folders() or not os.path.exists(tags_path)): #check if a project is open and the .tags file exists
+            return results
+        f=os.popen("grep -i '^"+prefix+"' '"+tags_path+"' | awk '{ print $1 }'") # grep tags from project directory .tags file
+        for i in f.readlines():
+            results.append([i.strip()])
+        results = [(item,item) for sublist in results for item in sublist] #flatten
+        results = list(set(results)) # make unique
+        results.sort() # sort
+        return results
 
 ##################################### TEST #####################################
 
