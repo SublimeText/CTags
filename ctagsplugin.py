6d131d8932d4b853a6c5609c8a2900e6f04c28c3
feng.D <fengidri@gmail.com>
2014-03-06 10:30:55 +0800

    Update ctagsplugin.py
    when jump to one function name. The default action will select the function name. This is not good when you are in vim mode.

--- ctagsplugin.py
+++ ctagsplugin.py
@@ -92,6 +92,10 @@ def escape_regex(s):
 
 
 def select(view, region):
+    a = region.a
+    b = region.b
+    a = min(a, b)
+    region = sublime.Region(a , a )
     sel_set = view.sel()
     sel_set.clear()
     sel_set.add(region)
