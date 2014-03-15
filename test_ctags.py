28d339e4f1d817b601a24aa48a8e1d7ec8e8f59f
Stephen Finucane <stephenfinucane@hotmail.com>
2014-03-15 18:21:22 +0000

    Replace calls to 'unittest' with 'unittest2'
    Backport new 'unittest' features to Python 2.6.5 (ST2)

--- test_ctags.py
+++ test_ctags.py
@@ -3,9 +3,14 @@
 """Unit tests for ctags.py"""
 
 import os
+import sys
 import tempfile
-import unittest
 import codecs
+
+if sys.version_info < (2, 7):
+    import unittest2 as unittest
+else:
+    import unittest
 
 try:
     import sublime
